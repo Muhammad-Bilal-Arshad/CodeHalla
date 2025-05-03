ce2e993f1329c9f8029b11b207affac14431c088
Bilal <muhammadbilalarshad490@gmail.com>
2025-05-03 22:50:34 +0500

    full backend structure setup

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,10 @@
 import express from 'express';
-
+import { login, signup, logout } from '../controllers/auth.controller.js';
 const router = express.Router();
+
+router.post('/login',  signup);
+router.post('/signup', login);
+router.post('/logout', logout);
+
 
 export default router;
